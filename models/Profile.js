8ab5387cbc23abbbee0fa967d5f4e0e0ff43ea6b
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2020-07-03 16:24:46 +0300

    Profile model edited.

--- models/Profile.js
+++ models/Profile.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
-const ProfileSchema = new mongoose.Schema({
+const Schema = mongoose.Schema;
+const ProfileSchema = new Schema({
   user: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: Schema.Types.ObjectId,
     ref: 'user',
   },
   company: {
