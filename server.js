1b4a13e75f3de4bc0efb2fd95d0649644f177e80
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2020-07-03 13:55:15 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => res.send('API RUNNING'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
