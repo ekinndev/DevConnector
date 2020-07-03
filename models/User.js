300965397b6b23b09bbf315156825f1593568323
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2020-07-03 15:56:58 +0300

    User timestamp added more elegant way.

--- models/User.js
+++ models/User.js
@@ -1,26 +1,25 @@
 const mongoose = require('mongoose');
-const Schema=mongoose.Schema;
-const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const Schema = mongoose.Schema;
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  avatar: {
-    type: String,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = User = mongoose.model('user', UserSchema);
