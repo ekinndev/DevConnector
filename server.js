85b26645b4a0b4e4feb1a5f45dc39bd67fb99670
Ekin Abalıoğlu <abaliogluekin@gmail.com>
2020-07-15 12:24:06 +0300

    Ready to heroku

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 connectDB();
 //Init Middleware
-app.use(express.json({extended:false}));
-app.get('/', (req, res) => res.send('API RUNNING'));
+app.use(express.json({ extended: false }));
 
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
+//Serve Static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.send(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
